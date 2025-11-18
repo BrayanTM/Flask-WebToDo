7f0bb7dfbc374a12a0481f29af635982318cc940
Brayan Tebelan <brayantebelan@gmail.com>
2025-11-18 16:41:14 -0600

    feat: Implementar gestión de autenticación y tareas en la aplicación Flask

--- todoapp/auth.py
+++ todoapp/auth.py
@@ -1,16 +1,12 @@
-from flask import Blueprint, render_template, request, url_for, redirect, flash
+from flask import Blueprint, render_template, request, url_for, redirect, flash, session, g
 from werkzeug.security import generate_password_hash, check_password_hash
+import functools
 from .models import User
 from .extensions import db
 import os
 
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
-
-
-@auth_bp.route('/login')
-def login():
-    return render_template('auth/login.html')
 
 
 @auth_bp.route('/register', methods=['GET', 'POST'])
@@ -33,4 +29,51 @@ def register():
         flash('User registered successfully!', 'success')
         return redirect(url_for('auth.login'))
 
+    if g.user:
+        return redirect(url_for('todo.index'))
     return render_template('auth/register.html')
+
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            session.clear()
+            session['user_id'] = user.id
+            flash('Inicio de sesión exitoso!', 'success')
+            return redirect(url_for('todo.index'))
+        else:
+            flash('Nombre de usuario o contraseña inválidos', 'error')
+    
+    if g.user:
+        return redirect(url_for('todo.index'))
+    return render_template('auth/login.html')
+
+
+@auth_bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+@auth_bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get_or_404(user_id)
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
